8c7f2262f3b7a0e2af0ebafe50d9cd894911ef2c
Idriss Neumann <idriss.neumann@comwork.io>
2022-07-07 22:41:19 +0100

    Add prices.env file

--- price_calculator/price_calculator.sh
+++ price_calculator/price_calculator.sh
@@ -15,4 +15,11 @@ $SED_BIN -i.tmp "s/\([0-9]\),\([0-9]*\)/\1.\2/g;s/\"//g" "${DATA_FILE}"
 $AWK_BIN -F ',' 'BEGIN {sep=","} {print $1""sep""$2""sep""$3""sep""$4""sep""$5}' "${DATA_FILE}.tmp" > "${DATA_FILE}.tmp2"
 $AWK_BIN '{if ($0 ~ "V_ARRAY_PRICING"){system("mdtable '"${DATA_FILE}.tmp2"' 2>/dev/null || :")} else {print $0}}' "${ROOT_DIR}/sizing_pricing.tpl" > "sizing_pricing.md"
 
+declare -a SIZES
+SIZES=("S", "L", "M", "XL")
+echo "#!/usr/bin/env bash" > prices.env
+for letter in "${SIZES[@]}"; do
+  echo "export PRICE_DEV1_${letter}=$(cat ${DATA_FILE}.tmp2|$AWK_BIN '($1 == "DEV-'"${letter}"'"){print $4}')" >> prices.env
+done
+
 rm -rf "${DATA_FILE}".tmp*
