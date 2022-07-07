03ca276d59ab2960c2e34783a3be4a1219a24679
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2022-07-07 21:53:31 +0000

    Update pricing and push

--- prices_env.sh
+++ prices_env.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-## This script is generated, do not update it manually! See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main/price_calculator/README.md
+## This script is generated, do not update it manually!
+## See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main/price_calculator/README.md
 
 export PRICE_DEV1_S="0.022"
 export PRICE_DEV1_L="0.088"
