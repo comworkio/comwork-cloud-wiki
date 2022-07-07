1c90f11115422540ca4a823ff3da4051af8ce46d
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2022-07-07 21:50:07 +0000

    Update pricing and push

--- prices_env.sh
+++ prices_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-export PRICE_DEV1_S=
-export PRICE_DEV1_L=
-export PRICE_DEV1_M=
-export PRICE_DEV1_XL=
+
+export PRICE_DEV1_S=""
+export PRICE_DEV1_L=""
+export PRICE_DEV1_M=""
+export PRICE_DEV1_XL=""
