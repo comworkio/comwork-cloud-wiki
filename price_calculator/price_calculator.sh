e596e944e61f6e35fd4296f26d1653de1d9bf76b
Idriss Neumann <neumann.idriss@gmail.com>
2022-07-08 10:41:03 +0000

    Update price_calculator/sizing_pricing.tpl, price_calculator/price_calculator.sh

--- price_calculator/price_calculator.sh
+++ price_calculator/price_calculator.sh
@@ -14,6 +14,7 @@ DATA_FILE="${ROOT_DIR}/data.csv"
 $SED_BIN -i.tmp "s/\([0-9]\),\([0-9]*\)/\1.\2/g;s/\"//g" "${DATA_FILE}"
 $AWK_BIN -F ',' 'BEGIN {sep=","} {print $1""sep""$2""sep""$3""sep""$4""sep""$5}' "${DATA_FILE}.tmp" > "${DATA_FILE}.tmp2"
 $AWK_BIN '{if ($0 ~ "V_ARRAY_PRICING"){system("mdtable '"${DATA_FILE}.tmp2"' 2>/dev/null || :")} else {print $0}}' "${ROOT_DIR}/sizing_pricing.tpl" > "sizing_pricing.md"
+sed -i "s/V_DATE/$(date +"%Y-%m-%d")/g"
 
 declare -a SIZES
 SIZES=("S" "L" "M" "XL")
