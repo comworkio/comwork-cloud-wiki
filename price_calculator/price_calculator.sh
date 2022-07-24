cf5a41019575668e09cb6d7479938c952e4037aa
Idriss Neumann <idriss.neumann@comwork.io>
2022-07-24 10:22:07 +0100

    Update price labels

--- price_calculator/price_calculator.sh
+++ price_calculator/price_calculator.sh
@@ -25,7 +25,7 @@ echo "## See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main
 
 echo "" >> prices_env.sh
 for letter in "${SIZES[@]}"; do
-  price=$($AWK_BIN -F ',' '($1 == "DEV-'"${letter}"'"){print $4}' "${DATA_FILE}.tmp2"|grep -Eo "[0-9\.]*")
+  price=$($AWK_BIN -F ',' '($1 == "DEV1-'"${letter}"'"){print $4}' "${DATA_FILE}.tmp2"|grep -Eo "[0-9\.]*")
   echo "export PRICE_DEV1_${letter}=\"${price}\"" >> prices_env.sh
 done
 
