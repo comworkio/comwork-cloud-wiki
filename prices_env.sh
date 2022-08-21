f4818148435d060e9315b1b141d0505db933ce3a
Amir Ghedira <amirghedira06@gmail.com>
2022-08-21 21:33:18 +0000

    Update prices_env.sh

--- prices_env.sh
+++ prices_env.sh
@@ -4,13 +4,13 @@
 ## See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main/price_calculator/README.md
 
 ## scaleway provider
-export PRICE_SCW_DEV1_S="0.022"
-export PRICE_SCW_DEV1_L="0.088"
-export PRICE_SCW_DEV1_M="0.045"
-export PRICE_SCW_DEV1_XL="0.133"
+export PRICE_SCW_DEV1-S="0.022"
+export PRICE_SCW_DEV1-L="0.088"
+export PRICE_SCW_DEV1-M="0.045"
+export PRICE_SCW_DEV1-XL="0.133"
 ## ovh provider
-export PRICE_OVH_b2_7="0.022"
-export PRICE_OVH_b2_15="0.088"
-export PRICE_OVH_b2_30="0.045"
-export PRICE_OVH_b2_60="0.133"
-export PRICE_OVH_b2_120="0.133"
+export PRICE_OVH_b2-7="0.022"
+export PRICE_OVH_b2-15="0.088"
+export PRICE_OVH_b2-30="0.045"
+export PRICE_OVH_b2-60="0.133"
+export PRICE_OVH_b2-120="0.133"
