a10a633f7e2e6f4f95e0b5ee80e4e3a8a62b8993
Amir Ghedira <amirghedira06@gmail.com>
2022-08-21 21:09:33 +0000

    adding ovh prices

--- prices_env.sh
+++ prices_env.sh
@@ -3,7 +3,14 @@
 ## This script is generated, do not update it manually!
 ## See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main/price_calculator/README.md
 
-export PRICE_DEV1_S="0.022"
-export PRICE_DEV1_L="0.088"
-export PRICE_DEV1_M="0.045"
-export PRICE_DEV1_XL="0.133"
+## scaleway provider
+export PRICE_SCW_DEV1_S="0.022"
+export PRICE_SCW_L="0.088"
+export PRICE_SCW_M="0.045"
+export PRICE_SCW_XL="0.133"
+## ovh provider
+export PRICE_OVH_b2_7="0.022"
+export PRICE_OVH_b2_15="0.088"
+export PRICE_OVH_b2_30"0.045"
+export PRICE_OVH_b2_60="0.133"
+export PRICE_OVH_b2_120="0.133"
