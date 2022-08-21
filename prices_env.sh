37cb29e11b75d2adcddb84eddac7a2a77af3b7dd
Amir Ghedira <amirghedira06@gmail.com>
2022-08-21 21:10:21 +0000

    Update prices_env.sh

--- prices_env.sh
+++ prices_env.sh
@@ -5,12 +5,12 @@
 
 ## scaleway provider
 export PRICE_SCW_DEV1_S="0.022"
-export PRICE_SCW_L="0.088"
-export PRICE_SCW_M="0.045"
-export PRICE_SCW_XL="0.133"
+export PRICE_SCW_DEV1_L="0.088"
+export PRICE_SCW_DEV1_M="0.045"
+export PRICE_SCW_DEV1_XL="0.133"
 ## ovh provider
 export PRICE_OVH_b2_7="0.022"
 export PRICE_OVH_b2_15="0.088"
-export PRICE_OVH_b2_30"0.045"
+export PRICE_OVH_b2_30="0.045"
 export PRICE_OVH_b2_60="0.133"
 export PRICE_OVH_b2_120="0.133"
