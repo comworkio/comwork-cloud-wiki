75c8f63876496f60eb57329b29bb280dc4eb4bc4
Amir Ghedira <amirghedira06@gmail.com>
2022-10-14 10:08:15 +0000

    Update deliver.sh

--- ci/deliver.sh
+++ ci/deliver.sh
@@ -3,6 +3,6 @@
 source ./ci/compute-env.sh
 
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
-docker-compose -f docker-compose-build.yml build "${IMAGE_NAME}" --no-cache
+docker-compose -f docker-compose-build.yml build --no-cache "${IMAGE_NAME}"
 docker tag "${IMAGE_NAME}:latest" "${CI_REGISTRY}/${IMAGE_NAME}:${VERSION}"
 docker push "${CI_REGISTRY}/${IMAGE_NAME}:${VERSION}"
