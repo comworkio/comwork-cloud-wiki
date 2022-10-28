d8bfa90cb8cdd2a2803bb18c7676b2a357bc7d53
Idriss Neumann <idriss.neumann@comwork.io>
2022-10-28 18:15:06 +0100

    Add discord notif

--- ci/notif.sh
+++ ci/notif.sh
@@ -12,5 +12,16 @@ slack_notif() {
     fi
 }
 
+discord_notif() {
+    token="${1}"
+    if [[ $token ]]; then
+        message="${SLACK_USERNAME} has been successfully deployed! version = ${VERSION}"
+        endpoint="https://discord.com/api/webhooks/${token}/slack"
+        payload="{\"text\": \"${message}\", \"username\": \"${SLACK_USERNAME}\"}"
+        curl -X POST "${endpoint}" -H "Accept: application/json" -d "${payload}"
+    fi
+}
+
 slack_notif "${SLACK_TOKEN}"
 slack_notif "${SLACK_TOKEN_PUBLIC}"
+discord_notif "${DISCORD_TOKEN_PUBLIC}"
