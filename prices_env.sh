02ab18a28ef321b198338271b7e578448007a729
idriss.neumann@comwork.io <idriss.neumann@comwork.io>
2022-12-01 08:00:21 +0000

    Update pricing and push

--- prices_env.sh
+++ prices_env.sh
@@ -3,13 +3,13 @@
 ## This script is generated, do not update it manually!
 ## See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main/price_calculator/README.md
 
-export PRICE_DEV1_S=0.019
-export PRICE_DEV1_M=0.037
-export PRICE_DEV1_L=0.073
-export PRICE_DEV1_XL=0.111
-export PRICE_PLAY2_PICO=0.024
-export PRICE_PLAY2_NANO=0.046
-export PRICE_PLAY2_MICRO=0.092
+export PRICE_DEV1_S=0.024
+export PRICE_DEV1_M=0.044
+export PRICE_DEV1_L=0.085
+export PRICE_DEV1_XL=0.124
+export PRICE_PLAY2_PICO=0.029
+export PRICE_PLAY2_NANO=0.051
+export PRICE_PLAY2_MICRO=0.097
 export PRICE_d2_2=0.017
 export PRICE_d2_4=0.040
 export PRICE_d2_8=0.073
