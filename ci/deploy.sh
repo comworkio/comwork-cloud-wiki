b088add8be76f1629adf118a954cf89d5b7460bc
Idriss Neumann <idriss.neumann@comwork.io>
2024-02-10 09:46:21 +0100

    Fix: service name

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,5 +3,5 @@
 source ./ci/compute-env.sh
 
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
+docker rm -f "comwork_cloud_doc_prod" || :
 docker-compose -f "docker-compose-prod.yml" up -d --force-recreate "${SERVICE_NAME}"
-
