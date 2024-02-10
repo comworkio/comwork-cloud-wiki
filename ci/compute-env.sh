6f9c2c8aab2baf668a02904987de58d1c3be4217
Idriss Neumann <idriss.neumann@comwork.io>
2024-02-10 09:33:21 +0100

    Fix: change registry and images name

--- ci/compute-env.sh
+++ ci/compute-env.sh
@@ -2,6 +2,9 @@
 
 git pull origin "${CI_COMMIT_BRANCH}" || :
 
-export IMAGE_NAME="comwork_cloud_doc"
+export CI_REGISTRY="rg.fr-par.scw.cloud/cwcloud-ce-u7u1q0"
+export CWCLOUD_APP="doc"
+export IMAGE_NAME="cwcloud-${CWCLOUD_APP}"
+export SERVICE_NAME="comwork_cloud_${CWCLOUD_APP}"
 export VERSION="${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
-export DISCORD_USERNAME="comwork_cloud_doc"
+export DISCORD_USERNAME="${IMAGE_NAME}"
