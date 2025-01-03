c04671637a6aa8c9eb591abf7a5d888f4afc8b0c
Idriss Neumann <idriss.neumann@comwork.io>
2025-01-03 12:56:13 +0100

    Move website

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,4 +3,4 @@
 source ./ci/compute-env.sh
 
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
-docker-compose -f "docker-compose-prod.yml" up -d --force-recreate "${SERVICE_NAME}"
+docker compose -f "docker-compose-prod.yml" up -d --force-recreate "${SERVICE_NAME}"
