3b4e8ad1287329dafcf479bc6a39c4ea47427af8
Idriss Neumann <idriss.neumann@comwork.io>
2025-01-08 17:03:17 +0100

    New env file

--- price_calculator/price_calculator.sh
+++ price_calculator/price_calculator.sh
@@ -10,7 +10,7 @@ fi
 
 ROOT_DIR="price_calculator"
 DATA_FILE="${ROOT_DIR}/data.csv"
-PRICE_ENV_FILE="prices_env.sh"
+PRICE_ENV_FILE=".env.price"
 DOC_FILE="sizing_pricing.md"
 
 dos2unix "${DATA_FILE}" >/dev/null 2>&1 || :
@@ -19,14 +19,13 @@ $AWK_BIN -F ',' 'BEGIN {sep=","} {print $1""sep""$2""sep""$4""sep""$5""sep""$6""
 $AWK_BIN '{if ($0 ~ "V_ARRAY_PRICING"){system("mdtable '"${DATA_FILE}.tmp2"' 2>/dev/null || :")} else {print $0}}' "${ROOT_DIR}/sizing_pricing.tpl" > "${DOC_FILE}"
 $SED_BIN -i "s/V_DATE/$(date +"%Y-%m-%d")/g" "${DOC_FILE}"
 
-echo "#!/usr/bin/env bash" > "${PRICE_ENV_FILE}"
 echo "" >> "${PRICE_ENV_FILE}"
 echo "## This script is generated, do not update it manually!" >> "${PRICE_ENV_FILE}"
 echo "## See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main/price_calculator/README.md" >> prices_env.sh
 echo "" >> "${PRICE_ENV_FILE}"
 
-$AWK_BIN -F ',' 'BEGIN {sep=","} {if($3 != "Variable"){gsub(/ *€/,"", $5); print "export "$3"="$5}}' "${DATA_FILE}.tmp" >> "${PRICE_ENV_FILE}"
+$AWK_BIN -F ',' 'BEGIN {sep=","} {if($3 != "Variable"){gsub(/ *€/,"", $5); print $3"="$5}}' "${DATA_FILE}.tmp" >> "${PRICE_ENV_FILE}"
 
-echo "export TTVA=1.2" >> "${PRICE_ENV_FILE}"
+echo "TTVA=1.2" >> "${PRICE_ENV_FILE}"
 
 rm -rf "${DATA_FILE}".tmp*
