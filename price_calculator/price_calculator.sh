e9370aace8d88a12809c7e930054621d3cb81be1
Idriss Neumann <idriss.neumann@comwork.io>
2025-01-08 17:18:33 +0100

    Generate new env file

--- price_calculator/price_calculator.sh
+++ price_calculator/price_calculator.sh
@@ -19,8 +19,7 @@ $AWK_BIN -F ',' 'BEGIN {sep=","} {print $1""sep""$2""sep""$4""sep""$5""sep""$6""
 $AWK_BIN '{if ($0 ~ "V_ARRAY_PRICING"){system("mdtable '"${DATA_FILE}.tmp2"' 2>/dev/null || :")} else {print $0}}' "${ROOT_DIR}/sizing_pricing.tpl" > "${DOC_FILE}"
 $SED_BIN -i "s/V_DATE/$(date +"%Y-%m-%d")/g" "${DOC_FILE}"
 
-echo "" >> "${PRICE_ENV_FILE}"
-echo "## This env file is generated, do not update it manually!" >> "${PRICE_ENV_FILE}"
+echo "## This env file is generated, do not update it manually!" > "${PRICE_ENV_FILE}"
 echo "## See: https://gitlab.comwork.io/comwork_public/comwork_cloud/-/blob/main/price_calculator/README.md" >> prices_env.sh
 echo "" >> "${PRICE_ENV_FILE}"
 
