372bd9e0b1248c9914a0abe33c725e6428323469
Idriss Neumann <idriss.neumann@comwork.io>
2025-01-18 00:39:11 +0100

    Remove docker container

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,5 +2,6 @@
 
 source ./ci/compute-env.sh
 
+docker ps -a|awk '($0 ~ "comwork_wiki_prod"){system("docker rm -f "$1)}' || :
 docker login "${CI_REGISTRY}" --username "${CI_REGISTRY_USER}" --password "${CI_REGISTRY_PASSWORD}"
 docker compose -f "docker-compose-prod.yml" up -d --force-recreate "${SERVICE_NAME}"
